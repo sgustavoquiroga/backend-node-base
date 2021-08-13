3f70798154ca798b6c8e1861c83242aaaacf1883
Gustavo Quiroga <sgustavoquiroga@gmail.com>
2021-08-13 14:19:49 -0300

    :construction: set td

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 const config = {
-    verbose: true,
     testEnvironment: "node",
-    moduleFileExtensions : ["js", "jsx", "json", "node"]
+    moduleFileExtensions : ["js", "jsx", "json", "node"],
   };
 export default config;
--- test/unit/services/user.service.test.ts
+++ test/unit/services/user.service.test.ts
@@ -1,6 +1,8 @@
 import { UserService } from '../../../src/services';
 import { UserRepositoryMock } from '../../mocks';
 import UserModelMock from '../../mocks/user/user.model.mock';
+import db from '../../../src/config/connection';
+
 const { user, users } = UserModelMock;
 
 describe("User Service", () => {
@@ -16,4 +18,5 @@ describe("User Service", () => {
         const expected = await _userService.get(user.id);
         expect(expected).toMatchObject(user);
     });
+    db.close();
 });
